4f2f2230d90843181cbd8b82ad227dbb23bc5b59
Aisha <ishaarigbabu@gmail.com>
2021-09-23 09:14:42 -0500

    different approach to wave 3 uses single keys

--- adagrams/game.py
+++ adagrams/game.py
@@ -69,7 +69,98 @@ def uses_available_letters(word, letter_bank):
         print("please enter alphabets")
 
 def score_word(word):
-    pass
+
+    score = 0
+
+    # score_chart = {
+    #     1 : ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    #     2 : ["D", "G"],
+    #     3 : ["B", "C", "M", "P"],
+    #     4 : ["F", "H", "V", "W", "Y"],
+    #     5 : ["K"],
+    #     8 : ["J","X"],
+    #     10 : ["Q", "Z"]
+    # }
+
+    # score_chart = {
+    #     ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T") : 1,
+    #     ("D", "G") : 2,
+    #     ("B", "C", "M", "P") : 3,
+    #     ("F", "H", "V", "W", "Y") :4,
+    #     ("K") : 5,
+    #     ("J","X") : 8,
+    #     ("Q", "Z") : 10
+
+    #}
+
+    score_chart = {"A": 1, 
+        "E" : 1,
+        "I" : 1,
+        "O" : 1,
+        "U" : 1, 
+        "L" : 1, 
+        "N" : 1, 
+        "R" : 1,
+        "S" : 1,
+        "T" : 1,
+        "D" : 2,
+        "G" : 2,
+        "B" : 3,
+        "C" : 3,
+        "M" : 3, 
+        "P" : 3,
+        "F" : 4, 
+        "H" : 4,
+        "V" : 4,
+        "W" : 4,
+        "Y" : 4,
+        "K" : 5,
+        "J" : 8,
+        "X" : 8,
+        "Q" : 10,
+        "Z" : 10,
+    }
+
+    word = word.upper()
+    for char in word:
+        score += score_chart[char]
+
+    if len(word) >= 7:
+        score += 8
+    
+    return score 
+
+    # if not word.isupper():
+    #         word = word.upper()
+
+    # if word:
+    #     for char in word:
+    #         for point, list_of_letters in score_chart.items():
+    #             if char in list_of_letters:
+    #                 score += point
+        
+    #     if 7 <= len(word) <= 10:
+    #         score += 8
+
+    # letter_values = []
+    # word = word.upper()
+    # for char in word:
+    #     for point, letter_list in score_chart.items():
+    #         if char in letter_list:
+    #             letter_values.append(point)
+                
+    # score = sum(letter_values)
+    # if 7 <= len(word) <= 10:
+    #     score += 8
+
+    # return score
+
+
+
+
+
+
+
 
 def get_highest_word_score(word_list):
     pass
